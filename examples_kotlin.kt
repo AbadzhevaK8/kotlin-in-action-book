816585a0d5fab9fa1457ed2549bed53076160553
Makemeloco <make.me.loco@gmail.com>
2021-04-17 01:29:52 +0300

    #2.4.
    #2.4.

--- examples_kotlin.kt
+++ examples_kotlin.kt
@@ -1,4 +1,4 @@
-#1.1
+#1.1.
 
 data class Person(val name: String,
                  val age: Int? = null)
@@ -57,13 +57,13 @@ verticalLayout {
 ==============================================
 
 В простейшем случае скомпилировать код из
-командной строки можно с помощью команды kotlinc, а запустить - ко­мандой java:
+командной строки можно с помощью командbl kotlinc, а запустить - ко­мандой java:
 
-kotlinc <исходный файл или каталог> -include-runtime -d <имя jаr-файла>
+kotlinc <исходнblй файл или каталог> -include-runtime -d <имя jаr-файла>
 java -jar <имя jаr-файла>
 
 ==============================================
-
+#2.1.
 fun main() {
     println("Hello, world!")
 }
@@ -76,21 +76,69 @@ fun main(){
 	}
     println(max(1, 2))
 }
+----------------------
+fun main(){
+    fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+    println(max(13, 2))
+}
+----------------------
+fun main(){
+    fun max(a: Int, b: Int) = if (a > b) a else b
+
+    println(max(13, 27))
+}
 
 ==============================================
-
+val question = "The Ultimate Question of Life, the Universe, and Everything."
+val answer = 42
+val answer: Int = 42
 ==============================================
-
+val message: String
+if (canPerformOperation()) {
+message = "Success"
+// ... вblполнить операцию
+}
+else {
+message = "Failed"
+}
 ==============================================
-
+val languages = arrayListOf("Java")
+languages.add("Kotlin")
 ==============================================
-
+#2.2.
+fun main(args: Array<String>) {
+	val name = if (args.size > 0) args[0] else "Kotlin"
+  println("Hello, $name!")
+}
 ==============================================
-
+fun main(args: Array<String>) {
+	if (args.size > 0) {
+		println("Hello, ${args[0]}!")
+	}
+}
 ==============================================
-
+fun main(args: Array<String>) {
+println("Hello, ${if (args.size > 0) args[0] else "someone"}!")
+}
 ==============================================
+#2.3.
+/* Java */
+public class Person {
+  private final String name;
 
+  public Person(String name) {
+    this.name = name;
+  }
+  public String getName() {
+    return name;
+  }
+}
+----------------------
+#2.4.
+fun main(args: Array<String>) {
+	class Person(val name: String)
+}
 ==============================================
 
 ==============================================
