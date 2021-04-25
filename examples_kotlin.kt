2e679cd5b2819df33f88ab2a2150c61c666d5844
Makemeloco <make.me.loco@gmail.com>
2021-04-26 00:23:40 +0300

    feat: Add p.75 example.

--- examples_kotlin.kt
+++ examples_kotlin.kt
@@ -528,7 +528,120 @@ fun main(args: Array<String>) {
     readNumber(reader)
 }
 ==============================================
+package ch03.ex1_1_CreatingCollectionsInKotlin
 
+val set = hashSetOf(1, 7, 53)
+val list = arrayListOf(1, 7, 53)
+val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
+
+fun main(args: Array<String>) {
+    println(set.javaClass)
+    println(list.javaClass)
+    println(map.javaClass)
+}
+==============================================
+package ch03.ex1_2_CreatingCollectionsInKotlin1
+
+fun main(args: Array<String>) {
+    val strings = listOf("first", "second", "fourteenth")
+    println(strings.last())
+    val numbers = setOf(1, 14, 2)
+    println(numbers.max())
+}
+==============================================
+package ch03.ex2_1_MakingFunctionsEasierToCall
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(list)
+}
+==============================================
+package ch03.JoinToString
+
+fun <T> joinToString(
+        collection: Collection<T>,
+        separator: String,
+        prefix: String,
+        postfix: String
+): String {
+
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(joinToString(list, "; ", "(", ")"))
+}
+==============================================
+package ch03.JoinToStringFinal
+
+fun <T> Collection<T>.joinToString(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(list.joinToString(separator = "; ",
+          prefix = "(", postfix = ")"))
+}
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
+==============================================
 
 
 
