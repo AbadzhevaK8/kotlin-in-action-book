a6953984c4c5db296f1ac5888d0bd565d128af46
Makemeloco <make.me.loco@gmail.com>
2021-04-28 00:53:57 +0300

    feat: Add page 83.

--- examples_kotlin.kt
+++ examples_kotlin.kt
@@ -657,7 +657,38 @@ fun main(args: Array<String>) {
     println(listOf("one", "two", "eight").join(" "))
 }
 ==============================================
+package ch03.ex3_4_1_NoOverridingForExtensionFunctions
+
+open class View {
+    open fun click() = println("View clicked")
+}
+
+class Button: View() {
+    override fun click() = println("Button clicked")
+}
+
+fun main(args: Array<String>) {
+    val view: View = Button()
+    view.click()
+}
 ==============================================
+package ch03.ex3_4_2_NoOverridingForExtensionFunctions1
+
+open class View {
+    open fun click() = println("View clicked")
+}
+
+class Button: View() {
+    override fun click() = println("Button clicked")
+}
+
+fun View.showOff() = println("I'm a view!")
+fun Button.showOff() = println("I'm a button!")
+
+fun main(args: Array<String>) {
+    val view: View = Button()
+    view.showOff()
+}
 ==============================================
 ==============================================
 ==============================================
