21225f97c7434a663d5df99589298b6864f40401
Makemeloco <make.me.loco@gmail.com>
2021-04-29 00:47:21 +0300

    feat:

--- examples_kotlin.kt
+++ examples_kotlin.kt
@@ -690,7 +690,30 @@ fun main(args: Array<String>) {
     view.showOff()
 }
 ==============================================
+package ch03.ex3_5_ExtensionProperties
+
+val String.lastChar: Char
+    get() = get(length - 1)
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value: Char) {
+        this.setCharAt(length - 1, value)
+    }
+
+fun main(args: Array<String>) {
+    println("Kotlin".lastChar)
+    val sb = StringBuilder("Kotlin?")
+    sb.lastChar = '!'
+    println(sb)
+}
 ==============================================
+package strings
+
+fun String.lastChar(): Char = this.get(this.length - 1)
+
+fun main(args: Array<String>) {
+    println("Kotlin".lastChar())
+}
 ==============================================
 ==============================================
 ==============================================
